e6472d155169c06b152e0e1c8a8a63755768ce45
lehoan1810 <hoanhao18102000@gmail.com>
2022-07-25 09:26:44 +0700

    update

---
+++ src/components/common/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { TiGroupOutline } from "react-icons/ti";
+import { BiMessageRoundedDots } from "react-icons/bi";
+import { BsArrowDownCircle } from "react-icons/bs";
+
+const Navbar = () => {
+	return <div className="navigation"></div>;
+};
+
+export default Navbar;
---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div className="container header-container">
+			<h3>
+				<p>
+					I <span>'</span>m
+				</p>
+				Libra
+				<h2>Xin Chào Mọi Người</h2>
+			</h3>
+		</div>
+	);
+};
+
+export default Header;
